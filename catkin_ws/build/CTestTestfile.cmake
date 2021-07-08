212fc8f32ed127c51f1e2bb56d9322f465f07f43
Fei Yu <ethan.fei.yu@gmail.com>
2021-07-08 04:17:46 +0000

    Create a basic robot for Project: Go Chase It!

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -1,9 +1,8 @@
 # CMake generated Testfile for 
-# Source directory: /home/workspace/catkin_ws/src
-# Build directory: /home/workspace/catkin_ws/build
+# Source directory: /home/workspace/udacity-robotics-software-engineer/catkin_ws/src
+# Build directory: /home/workspace/udacity-robotics-software-engineer/catkin_ws/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
-subdirs(fisrt_package)
-subdirs(simple_arm)
+subdirs(my_robot)
