b57c33d97faa5f7969bc650b9e6142ca65a73ae3
Fei Yu <ethan.fei.yu@gmail.com>
2021-07-19 04:45:59 +0000

    Create drive_bot node.

---
+++ catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -0,0 +1,54 @@
+#include <ros/ros.h>
+#include "geometry_msgs/Twist.h"
+#include "ball_chaser/DriveToTarget.h"
+
+class DriveBot
+{
+public:
+  DriveBot()
+  {
+    //Topic you want to publish
+    pub_ = n_.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+    //Topic you want to subscribe
+    srv_ = n_.advertiseService("/ball_chaser/DriveToTarget", &DriveBot::handle_drive_request_callback, this);
+  }
+
+  bool handle_drive_request_callback(ball_chaser::DriveToTarget::Request& req, ball_chaser::DriveToTarget::Response& res)
+  {
+    ROS_INFO("DriveToTargetRequest received linear_x:%1.2f, angular_z:%1.2f", (float)req.linear_x, (float)req.angular_z);
+
+    // Create a motor_command object of type geometry_msgs::Twist
+    geometry_msgs::Twist motor_command;
+    // Set wheel velocities
+    motor_command.linear.x = req.linear_x;
+    motor_command.angular.z = req.angular_z;
+    // Publish velocities to drive the robot
+    pub_.publish(motor_command);
+
+    // Return a response message
+    res.msg_feedback = "Motor command set - linear.x: " + std::to_string(motor_command.linear.x) + " , angular.z: " + std::to_string(motor_command.angular.z);
+    ROS_INFO_STREAM(res.msg_feedback);
+
+    return true;
+  }
+
+private:
+  ros::NodeHandle n_; 
+  ros::Publisher pub_;
+  ros::ServiceServer srv_;
+
+};//End of class DriveBot
+
+int main(int argc, char **argv)
+{
+  //Initiate ROS
+  ros::init(argc, argv, "drive_bot");
+
+  //Create an object of class DriveBot that will take care of everything
+  DriveBot DBObject;
+
+  ros::spin();
+
+  return 0;
+}
