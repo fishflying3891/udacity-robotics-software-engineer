d2c76f15200789c1cca93ca7de6b3b0ac7a54bd0
Fei Yu <ethan.fei.yu@gmail.com>
2021-09-08 03:10:32 +0000

    Build sucessfully.

--- Project4/build/catkin_generated/setup_cached.sh
+++ Project4/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,7 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/home/workspace/udacity-robotics-software-engineer/Project4/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/root/catkin_ws/devel/lib:/usr/local/cuda-9.0/lib64:/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/local/lib/x86_64-linux-gnu:/usr/local/lib/i386-linux-gnu:/usr/lib/x86_64-linux-gnu:/usr/lib/i386-linux-gnu:/usr/local/nvidia/lib:/usr/local/nvidia/lib64"
-export PATH="/opt/ros/kinetic/bin:/root/catkin_ws/devel/bin:/usr/bin:/usr/local/cuda-9.0/bin:/root/miniconda3/bin:/opt/pytorch/bin:/opt/utils/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/VirtualGL/bin:/opt/TurboVNC/bin:/opt/swift/swift-4.0.3-RELEASE-ubuntu16.04/usr/bin"
+export CMAKE_PREFIX_PATH="/home/workspace/udacity-robotics-software-engineer/Project4/devel:/opt/ros/kinetic:/root/catkin_ws/devel/"
+export LD_LIBRARY_PATH="/home/workspace/udacity-robotics-software-engineer/Project4/devel/lib:/root/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda-9.0/lib64:/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/local/lib/x86_64-linux-gnu:/usr/local/lib/i386-linux-gnu:/usr/lib/x86_64-linux-gnu:/usr/lib/i386-linux-gnu:/usr/local/nvidia/lib:/usr/local/nvidia/lib64"
+export PATH="/root/catkin_ws/devel/bin:$PATH"
 export PWD="/home/workspace/udacity-robotics-software-engineer/Project4/build"
-export PYTHONPATH="/home/workspace/udacity-robotics-software-engineer/Project4/devel/lib/python2.7/dist-packages:$PYTHONPATH"
--- Project4/devel/_setup_util.py
+++ Project4/devel/_setup_util.py
@@ -270,7 +270,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = '/home/workspace/udacity-robotics-software-engineer/Project4/devel;/opt/ros/kinetic;/root/catkin_ws/devel'.split(';')
+            CMAKE_PREFIX_PATH = '/home/workspace/udacity-robotics-software-engineer/Project4/devel;/root/catkin_ws/devel/;/opt/ros/kinetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
